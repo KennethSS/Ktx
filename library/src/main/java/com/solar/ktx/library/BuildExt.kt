b0af5c67e872e9dfff0a6f743a3c268cacede487
Kenneth <wlsrhkd4023@gmail.com>
2021-07-20 14:11:55 +0900

    add: Build Os atLeast P, Q, R

--- library/src/main/java/com/solar/ktx/library/BuildExt.kt
+++ library/src/main/java/com/solar/ktx/library/BuildExt.kt
@@ -1,3 +1,6 @@
 package com.solar.ktx.library
 
 fun atLeastOreo() = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O
+fun atLeastPie() = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P
+fun atLeast10() = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+fun atLeast11() = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R
